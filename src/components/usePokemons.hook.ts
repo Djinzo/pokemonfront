48d000b5c5beb64c3daa717378e13ec2e79247db
Mohamed GOULZIMA <MGoulzima@externe.ma>
2023-09-18 08:43:55 +0100

    in commont usePokemons

--- src/components/usePokemons.hook.ts
+++ src/components/usePokemons.hook.ts
@@ -1,21 +1,21 @@
-// import { useDispatch } from "react-redux";
-// import { fetchPokemonsStart } from "../redux/pokemon";
+import { useDispatch } from "react-redux";
+import { fetchPokemonsStart } from "../redux/pokemon";
 import { useEffect } from "react";
-// import { usePokemonsSelector } from "../redux/pokemon/selectors";
+import { usePokemonsSelector } from "../redux/pokemon/selectors";
 
 export const usePokemons = () => {
-  // const pokemon = usePokemonsSelector();
-  // const dispatch = useDispatch();
+  const pokemon = usePokemonsSelector();
+  const dispatch = useDispatch();
 
   const fetchMore = () => {
-    // dispatch(fetchPokemonsStart({ page: pokemon.page }));
+    dispatch(fetchPokemonsStart({ page: pokemon.page }));
   };
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(fetchMore, []);
 
   return {
-    pokemon: [],
+    pokemon,
     fetchMore,
   };
 };
